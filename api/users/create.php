b98d212fbef20d944fe369c156e9270d74ad4201
vanessnat <natachatwyffels@gmail.com>
2023-02-08 09:48:56 +0100

    commit

--- api/users/create.php
+++ api/users/create.php
@@ -2,14 +2,14 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php' ; 
 
-$numMemb = $_POST['numMemb'];
-$prenomMemb = $_POST['prenomMemb'];
-$nomMemb = $_POST['nomMemb'];
-$pseudoMemb = $_POST['pseudoMemb'];
-$passMemb = $_POST['passMemb'];
-$eMailMemb = $_POST['eMailMemb'];
-$dtCreaMemb = $_POST['dtCreaMemb'];
-$numStat = $_POST['numStat'];
+$numMemb = sql_escape($_POST['numMemb']);
+$prenomMemb =  sql_escape($_POST['prenomMemb']);
+$nomMemb = sql_escape($_POST['nomMemb']);
+$pseudoMemb = sql_escape($_POST['pseudoMemb']);
+$passMemb = sql_escape($_POST['passMemb']);
+$eMailMemb = sql_escape($_POST['eMailMemb']);
+$dtCreaMemb = sql_escape($_POST['dtCreaMemb']);
+$numStat = sql_escape($_POST['numStat']);
 
 sql_insert('membre', "numMemb, prenomMemb, nomMemb, pseudoMemb, passMemb, eMailMemb, dtCreaMemb, numStat", "$numMemb, '$prenomMemb', '$nomMemb', '$pseudoMemb', '$passMemb', '$eMailMemb', '$dtCreaMemb', $numStat");
 
--- api/users/delete.php
+++ api/users/delete.php
@@ -2,7 +2,8 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 
 
-$numMemb = $_POST['numMemb'];
+$numMemb = sql_escape($_POST['numMemb']);
+
 
 sql_delete('article', "numMemb = $numMemb");
 

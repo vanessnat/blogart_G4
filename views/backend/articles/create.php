486cccab1ad12f9e2648dc3394ddf7829af03213
chloaye <chloe.faye@mmibordeaux.com>
2023-02-09 12:48:37 +0100

    commit

--- views/backend/articles/create.php
+++ views/backend/articles/create.php
@@ -10,72 +10,71 @@ $thematiques = sql_select("thematique", "*");
 <div class="container-create">
     <div class="card text-bg bloc-article">
         <div class="card-img-overlay">
-            <h1 class="card-title text-center creat-article">Création d'articles</h1>
+            <h1 class="card-title text-center create-articles">Création d'articles</h1>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-12">
-            <!--Form to create a new article-->
-            <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post">
-                <div class="form-group">
-                    <label for="dtCreArt">Date</label>
-                    <input id="dtCreArt" class="form-control" type="datetime-local" name="dtCreArt">
-
-                    <label for="libTitrArt">Titre</label>
-                    <input id="libTitrArt" class="form-control" type="text" name="libTitrArt">
-
-                    <label for="libChapoArt">Chapô</label>
-                    <input id="libChapoArt" class="form-control" type="text" name="libChapoArt">
-
-                    <label for="libAccrochArt">Accroche</label>
-                    <input id="libAccrochArt" class="form-control" type="text-area" name="libAccrochArt">
+    <tbody>
+        <section>
+            <div class="row">
+                <div class="col-md-12">
+                    <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post">
+                        <div class="form-groupedit">
+                            <label for="dtCreArt">Date</label>
+                            <input id="dtCreArt" class="form-control" type="datetime-local" name="dtCreArt">
+                            <label for="libTitrArt">Titre</label>
+                            <input id="libTitrArt" class="form-control" type="text" name="libTitrArt">
+                            <label for="libChapoArt">Chapô</label>
+                            <input id="libChapoArt" class="form-control" type="text" name="libChapoArt">
+                            <label for="libAccrochArt">Accroche</label>
+                            <input id="libAccrochArt" class="form-control" type="text-area" name="libAccrochArt">
+                            <div class="create-para1">
+                                <label for="parag1Art">Paragraphe 1</label>
+                            </div>
+                            <div class="para-textarea">
+                                <textarea id="story" name="story"rows="5" cols="80">
+                                </textarea>
+                            </div>
+                            <label for="libSsTitr1Art">Sous-titre 1</label>
+                            <input id="libSsTitr1Art" class="form-control" type="text-area" name="libSsTitr1Art">
+                            <div class="create-para2">
+                                <label for="parag2Art">Paragraphe 2</label>
+                            </div>
+                            <div class="para-textarea">
+                                <textarea id="story" name="story"rows="5" cols="80">
+                                </textarea>
+                            </div>
+                            <label for="libSsTitr2Art">Sous-titre 2</label>
+                            <input id="libSsTitr2Art" class="form-control" type="text-area" name="libSsTitr2Art">
+                            <div class="create-para3">
+                                <label for="parag3Art">Paragraphe 3</label>
+                            </div>
+                            <div class="para-textarea">
+                                <textarea id="story" name="story"rows="5" cols="80">
+                                </textarea>
+                            </div>
+                            <label for="libConclArt">Conclusion</label>
+                            <input id="libConclArt" class="form-control" type="text" name="libConclArt">
+                            <label for="urlPhotArt">Illustration</label>
+                            <input id="urlPhotArt" class="form-control" type="file" name="urlPhotArt">
+                            <label for="numThem" class="title">Thématiques</label>  
+                            <select name="numThem" >
+                                <option value="">--Choisissez un thème--</option> 
+                                <?php foreach($thematiques as $thematique){ ?>
+                                    <option value="<?php echo $thematique['numThem']; ?>"><?php echo $thematique['libThem']; ?></option> 
+                                <?php } ?>
+                            </select>
+                        </div>
+                    </form>
                 </div>
-                <div class="form-group">
-                    <label for="parag1Art">Paragraphe 1</label>
-                    <textarea id="story" name="story"rows="5" cols="33">
-                    </textarea>
-
-                </div>
-                <div class="form-group">
-                    <label for="libSsTitr1Art">Sous-titre 1</label>
-                    <input id="libSsTitr1Art" class="form-control" type="text-area" name="libSsTitr1Art">
-                </div>
-                <div class="form-group">
-                    <label for="parag2Art">Paragraphe 2</label>
-                    <textarea id="story" name="story"rows="5" cols="33">
-                    </textarea>
-                </div>
-                <div class="form-group">
-                    <label for="libSsTitr2Art">Sous-titre 2</label>
-                    <input id="libSsTitr2Art" class="form-control" type="text-area" name="libSsTitr2Art">
-                </div>
-                <div class="form-group">
-                    <label for="parag3Art">Paragraphe 3</label>
-                    <textarea id="story" name="story"rows="5" cols="33">
-                    </textarea>
-                </div>
-                <div class="form-group">
-                    <label for="libConclArt">Conclusion</label>
-                    <input id="libConclArt" class="form-control" type="text" name="libConclArt">
-
-                    <label for="urlPhotArt">Illustration</label>
-                    <input id="urlPhotArt" class="form-control" type="file" name="urlPhotArt">
-
-                    <label for="numThem" class="title">Thématiques</label>  
-                    <select name="numThem" >
-                    <option value="">--Choisissez un thème--</option> 
-                    <?php foreach($thematiques as $thematique){ ?>
-                    <option value="<?php echo $thematique['numThem']; ?>"><?php echo $thematique['libThem']; ?></option> 
-                    <?php } ?>
-                    </select>
-
-                </div>
-                <div class="form-group mt-2">
-                    <button type="submit" class="btn btn-create">Créer l'article</button>
-                </div>
-            </form>
-        </div>
-    </div>
+            </div>
+        </section>
+        </section>
+        <section>
+            <div class="create-articles">
+                <a href="create.php" class="btn btn-create">Créer l'article</a>
+            </div>
+        </section>
+    </tbody>
 </div>
 
 <?php
--- views/backend/articles/update.php
+++ views/backend/articles/update.php
@@ -15,60 +15,85 @@ $thematiques = sql_select("thematique", "*");
 ?>
 
 
-<!--Bootstrap form to edit a new article-->
+<Body style="background-color: #ECECEC">
+<link rel="stylesheet" href="/src/css/style.css">
+<img src="/src/css/Accueil Image.png" class="img-fluid">
+
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Edit Articles</h1>
-        </div>
-        <div class="col-md-12">
-            <!--Form to edit a new article-->
-            <form action="<?php echo ROOT_URL . '/api/articles/update.php' ?>" method="post">
-                <div class="form-group">
-                    <input id="numArt" class="form-control" type="text" style="display: none" name="numArt" value="<?php echo ($article['numArt'])?>">
-                    <label for="dtCreArt">Date</label>
-                    <input id="dtCreArt" class="form-control" type="datetime-local" name="dtCreArt" value="<?php echo ($article['dtCreArt'])?>">
-                    <label for="libTitrArt">Titre</label>
-                    <input id="libTitrArt" class="form-control" type="text" name="libTitrArt" value="<?php echo ($article['libTitrArt'])?>">
-                    <label for="libChapoArt">Chapô</label>
-                    <input id="libChapoArt" class="form-control" type="text" name="libChapoArt" value="<?php echo ($article['libChapoArt'])?>">
-                    <label for="libAccrochArt">Accroche</label>
-                    <input id="libAccrochArt" class="form-control" type="text" name="libAccrochArt" value="<?php echo ($article['libAccrochArt'])?>">
-                    <label for="parag1Art">Paragraphe 1</label>
-                    <textarea id="story" name="story"rows="5" cols="33">
-                    <?php echo ($article['parag1Art'])?>
-                    </textarea>
-                    <label for="libSsTitr1Art">Sous-titre 1</label>
-                    <input id="libSsTitr1Art" class="form-control" type="text" name="libSsTitr1Art" value="<?php echo ($article['libSsTitr1Art'])?>">
-                    <label for="parag2Art">Paragraphe 2</label>
-                    <textarea id="story" name="story"rows="5" cols="33">
-                    <?php echo ($article['parag2Art'])?>
-                    </textarea>
-                    <label for="libSsTitr2Art">Sous-titre 2</label>
-                    <input id="libSsTitr2Art" class="form-control" type="text" name="libSsTitr2Art" value="<?php echo ($article['libSsTitr2Art'])?>">
-                    <label for="parag3Art">Paragraphe 3</label>
-
-                    <textarea id="story" name="story"rows="5" cols="33">
-                    <?php echo ($article['parag3Art'])?>
-                    </textarea>
-                    <label for="libConclArt">Conclusion</label>
-                    <input id="libConclArt" class="form-control" type="text" name="libConclArt" value="<?php echo ($article['libConclArt'])?>">
-                    <label for="urlPhotArt">Illustration</label>
-                    <input id="urlPhotArt" class="form-control" type="file" name="urlPhotArt" value="<?php echo ($article['urlPhotArt'])?>" accept="image/png, image/jpeg">
-                    <label for="numThem" class="title">Thématiques</label>  
-                    <select name="numThem" >
-                    <option value="<?php echo ($article['numThem'])?>"><?php echo ($article['libThem'])?></option> 
-                    <?php foreach($thematiques as $thematique){ ?>
-                    <option value="<?php echo $thematique['numThem']; ?>"><?php echo $thematique['libThem']; ?></option> 
-                    <?php } ?>
-                    </select>
-                    </select>
-
-                </div>
-                <div class="form-group mt-2">
-                    <button type="submit" class="btn btn-primary">Mettre à jour l'article</button>
-                </div>
-            </form>
+            <div class="bloc-article3">
+                <h1 class="choix-arttext">Modifier l'article</h1>
+            </div>
         </div>
     </div>
+    <tbody>
+        <section>
+            <div class="col-md-12">
+                <form action="<?php echo ROOT_URL . '/api/articles/update.php' ?>" method="post">
+                    <div class="form-groupedit">
+                        <input id="numArt" class="form-control" type="text" style="display: none" name="numArt" value="<?php echo ($article['numArt'])?>">
+                        <label for="dtCreArt">Date</label>
+                        <input id="dtCreArt" class="form-control" type="datetime-local" name="dtCreArt" value="<?php echo ($article['dtCreArt'])?>">
+                        <label for="libTitrArt">Titre</label>
+                        <input id="libTitrArt" class="form-control" type="text" name="libTitrArt" value="<?php echo ($article['libTitrArt'])?>">
+                        <label for="libChapoArt">Chapô</label>
+                        <input id="libChapoArt" class="form-control" type="text" name="libChapoArt" value="<?php echo ($article['libChapoArt'])?>">
+                        <label for="libAccrochArt">Accroche</label>
+                        <div class="update-para1">
+                            <input id="libAccrochArt" class="form-control" type="text" name="libAccrochArt" value="<?php echo ($article['libAccrochArt'])?>">
+                            <label for="parag1Art">Paragraphe 1</label>
+                            </div>
+                        <div class="para-textarea">
+                            <textarea id="story" name="story" rows="5" cols="80">
+                                <?php echo ($article['parag1Art'])?>
+                            </textarea>
+                        </div>
+                        <label for="libSsTitr1Art">Sous-titre 1</label>
+                        <input id="libSsTitr1Art" class="form-control" type="text" name="libSsTitr1Art" value="<?php echo ($article['libSsTitr1Art'])?>">
+                        <div class="udpate-para2">
+                            <label for="parag2Art">Paragraphe 2</label>
+                        </div>
+                        <div class="para-textarea">
+                            <textarea id="story" name="story"rows="5" cols="80">
+                                <?php echo ($article['parag2Art'])?>
+                            </textarea>
+                        </div>
+                        <label for="libSsTitr2Art">Sous-titre 2</label>
+                        <input id="libSsTitr2Art" class="form-control" type="text" name="libSsTitr2Art" value="<?php echo ($article['libSsTitr2Art'])?>">
+                        <div class="update-para3>
+                            <label for="parag3Art">Paragraphe 3</label>
+                        </div>
+                        <div class="para-textarea">
+                            <textarea id="story" name="story"rows="5" cols="80">
+                                <?php echo ($article['parag3Art'])?>
+                            </textarea>
+                        </div>
+                        <label for="libConclArt">Conclusion</label>
+                        <input id="libConclArt" class="form-control" type="text" name="libConclArt" value="<?php echo ($article['libConclArt'])?>">
+                        <label for="urlPhotArt">Illustration</label>
+                        <input id="urlPhotArt" class="form-control" type="file" name="urlPhotArt" value="<?php echo ($article['urlPhotArt'])?>" accept="image/png, image/jpeg">
+                        <label for="numThem" class="title">Thématiques</label>  
+                        <select name="numThem" >
+                        <option value="<?php echo ($article['numThem'])?>"><?php echo ($article['libThem'])?></option> 
+                        <?php foreach($thematiques as $thematique){ ?>
+                            <option value="<?php echo $thematique['numThem']; ?>"><?php echo $thematique['libThem']; ?></option> 
+                        <?php } ?>
+                        </select>
+                        </select>
+                    </div>
+                </form>
+            </div>
+        </section>
+        <section>
+            <div class="create-articles">
+                <a href="create.php" class="btn btn-update">Update</a>
+            </div>
+        </section>
+    </tbody>
 </div>
+
+<?php
+include '../../../footer.php'; // contains the footer
+
+?>

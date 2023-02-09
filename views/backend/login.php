b670c3ea823ae9e8f042e85719e97234ad044140
vanessnat <natachatwyffels@gmail.com>
2023-02-09 10:19:06 +0100

    commit

--- views/backend/login.php
+++ views/backend/login.php
@@ -11,26 +11,16 @@ include '../../header.php';
         </div>
         <div class="col-md-12">
             <!--Form to create a new member-->
-            <form action="<?php echo ROOT_URL . '/api/users/create.php' ?>" method="post">
+            <form action="<?php echo ROOT_URL . '/members/list.php' ?>" method="post">
                 <div class="form-group">
-                    <label for="prenomMemb">Prénom</label>
-                    <input id="prenomMemb" class="form-control" type="text" name="prenomMemb" required>
-                    <label for="nomMemb">Nom</label>
-                    <input id="nomMemb" class="form-control" type="text" name="nomMemb" required>
                     <label for="pseudoMemb">Pseudo</label>
                     <input id="pseudoMemb" class="form-control" type="text" name="pseudoMemb" required>
-                    <label for="pseudoMemb">Mot-de-passe</label>
-                    <input id="pseudoMemb" class="form-control" type="text" name="pseudoMemb" required>
-                    <label for="passMemb">Confimez le mot-de-passe</label>
-                    <input id="passMemb" class="form-control" type="password" name="passMemb"  minlength="8" required>
-                    <label for="eMailMemb">E-mail</label>
-                    <input id="eMailMemb" class="form-control" type="email" name="eMailMemb"  size="30" required>
-                    <label for="eMailMemb">Confirmez l'e-mail</label>
-                    <input id="eMailMemb" class="form-control" type="email" name="eMailMemb"  size="30" required>
+                    <label for="passMemb">Mot-de-passe</label>
+                    <input id="passMemb" class="form-control" type="text" name="passMemb" required>
                     <input id="numStat" style="display: none" class="form-control" type="text" name="numStat" >
                 </div>
                 <div class="form-group mt-2">
-                    <button type="submit" class="btn btn-primary">S'inscrire</button>
+                    <button type="submit" class="btn btn-primary">Se connecter</button>
                 </div>
             </form>
         </div>
